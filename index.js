d5b09b98aef8570375dca8531c954cff98808fe8
sgtpeppers <92605951+AtlasNavigator@users.noreply.github.com>
2021-11-09 21:43:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const readLine = require('readline-sync');
+
+// // Program 1
+// let a = 0;
+// while (a < 26) {
+//   console.log(a);
+//   a++; 
+// }
+
+// // Program 2
+// let b = 0;
+// while (b < 51) {
+//   console.log(b);
+//   b++; 
+// }
+
+// Program 3
+let c = -10;
+while (c < 10) {
+  console.log(c);
+  c++; 
+}
