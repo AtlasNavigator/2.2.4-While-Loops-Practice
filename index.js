f46813c551796e707db6b5d08149c895b820d508
Jose  Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2021-11-10 21:10:52 +0000

    Program 7 Baby!

--- index.js
+++ index.js
@@ -14,9 +14,43 @@ const readLine = require('readline-sync');
 //   b++; 
 // }
 
-// Program 3
-let c = -10;
-while (c < 10) {
-  console.log(c);
-  c++; 
-}
+// // Program 3
+// let c = -10;
+// while (c < 10) {
+//   console.log(c);
+//   c++; 
+// }
+
+// // Program 4
+// let d = 0;
+// while (d>-51) {
+//   console.log(d);
+//   d--; 
+// }
+
+// // Program 5
+// let e = 0;
+// while (e < 101) {
+//   if (e % 2 == 0){
+//     console.log(e);
+//   }
+//   e++;
+// }
+
+// // Program 6
+// let f = 100;
+// while (f > 0) {
+//   if (f % 2 == 1){
+//     console.log(f);
+//   }
+//   f--;
+// }
+
+// Program 7
+let g = 0;
+while (g < 100) {
+  if (g % 3 == 0){
+    console.log(g);
+  }
+  g++;
+};
