c6e37e26a53e136f02f581595040cccec2bba1cf
Jose  Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2021-11-10 22:04:51 +0000

    Maybe what I got done in class, today's been a rough day. I need a break.

--- index.js
+++ index.js
@@ -47,10 +47,88 @@ const readLine = require('readline-sync');
 // }
 
 // Program 7
-let g = 0;
-while (g < 100) {
-  if (g % 3 == 0){
-    console.log(g);
-  }
-  g++;
-};
+// let g = 0;
+// while (g < 100) {
+//   if (g % 3 == 0){
+//     console.log(g);
+//   }
+//   g++;
+// };
+
+// Program 8
+// let h = 0;
+// while (h <= 1000) {
+//   if (h % 5 == 0){
+//     console.log(h);
+//   }
+//   h++;
+// };
+
+// // Program 9 
+// let i = 0;
+// while (i < 100){
+//   if (i < 10){
+//     console.log(i);
+//   }else if (i > 25) {
+//     console.log(i);
+//   }
+//   i++; 
+// } 
+
+// // Program 10 (Ask Ms. Bolin, some issues.)
+// let j = 0;
+// while (j <= 100) {
+//   if (j < 10) {
+//     console.log (j);
+//   } else if (j > 20 && j < 40){
+//     console.log(j);
+//   } else if (j >50){
+//     console.log(j);
+//   }
+//   j++;
+// }
+
+// // Program 11
+// let userResponse = readLine.question('Would you like to quit this program (Y/N) ')
+// while (userResponse != "Y" && userResponse !='N'){
+//   console.log('Invalid Input'); // let person know you heard response
+//   userResponse = readLine.question('Would you like to quit this program (Y/N) ');
+// } //Force user into compliance, accepts inputs defined only
+
+// // Program 12
+// let userResponse = readLine.question('Would you like to quit this program (Y/y/N/n): ')
+// while (userResponse != "Y" && userResponse != "y" && userResponse != "n" && userResponse !='N'){
+//   console.log('Invalid Input'); // let person know you heard response
+//   userResponse = readLine.question('Would you like to quit this program (Y/y/N/n): ');
+// } //Force user into compliance, accepts inputs defined only
+
+// // Program 13
+// let userResponse = readLine.question('What do you choose, A, B, or C: ')
+// while (userResponse != "A" && userResponse != "B" && userResponse != "C"){
+//   console.log('Invalid Input'); // let person know you heard response
+//   userResponse = readLine.question('What do you choose, A, B, or C: ');
+// } //Force user into compliance, accepts inputs defined only
+
+// // Program 14
+// let userResponse = readLine.question('What do you choose, 1, 2, or 3: ')
+// while (userResponse != "1" && userResponse != "2" && userResponse != "3"){
+//   console.log('Invalid Input'); // let person know you heard response
+//   userResponse = readLine.question('What do you choose, 1, 2, or 3: ');
+// } //Force user into compliance, accepts inputs defined only
+
+// // Program 15
+// let userResponse = readLine.question('What do you choose (Anything 0-10): ')
+// while (userResponse != "0" && userResponse != "1" && userResponse != "2" && userResponse != "3" && userResponse != "4" && userResponse != "5" && userResponse != "6" && userResponse != "7" && userResponse != "8" && userResponse != "9" && userResponse != "10"){
+//   console.log('Invalid Input'); // let person know you heard response
+//   userResponse = readLine.question('What do you choose (Anything 0-10): ');
+// } //Force user into compliance, accepts inputs defined only
+
+// Program 16
+let num = Number;
+let userResponse = readLine.question('What do you choose (Positive Values): ')
+while (userResponse = Number < ('0'){
+  console.log('Invalid Input'); // let person know you heard response
+  userResponse = readLine.question('What do you choose (Anything 0-10): ');
+} //Force user into compliance, accepts inputs defined only
+
+}
